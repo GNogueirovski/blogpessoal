d4aac4303bc719c55bb5bfad93e355c30d75a395
Gabriel N <spbiel18@gmail.com>
2025-01-13 15:26:19 -0300

    Criação dos métodos getByID, getByTitulo, post, put e delete

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,17 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController 								// Define ao Spring que essa classe é uma Controller
 @RequestMapping("/postagens") 				   //Define qual endpoint vai ser tratado por essa classe
@@ -23,7 +24,41 @@ public class PostagemController{
 	@GetMapping // indica que esse método é chamado em Verbos/Métodos HTTP do tipo GET.
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
-		
+	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getByID(@PathVariable Long id) {
+		return postagemRepository.findById(id)
+				.map(postagem -> ResponseEntity.status(HttpStatus.OK).body(postagem))
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado o registro"));
+	}
+
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity <List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
+
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	}
+
+	@PutMapping
+	public ResponseEntity<Postagem> update(@Valid @RequestBody Postagem postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem)))
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado o registro"));
+
+	}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping ("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		if (postagem.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado o registro");
+		}
+		postagemRepository.deleteById(id);
 	}
 }
